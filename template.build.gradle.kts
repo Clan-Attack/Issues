0ef77d231fbe07eec5cfb6fde5c4ad86eb2f50c9
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2022-10-30 19:30:34 +0100

    Create template.build.gradle.kts

---
+++ template.build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.7.20"
+    id("io.papermc.paperweight.userdev") version "1.3.8"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "at.clanattack"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    paperDevBundle("1.19.2-R0.1-SNAPSHOT")
+}
+
+bukkit {
+    main = "at.clanattack.central.Central"
+    name = "Clanattack-<Name>"
+    version = "0.1"
+    apiVersion = "1.19"
+    author = "CheeseTastisch"
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
